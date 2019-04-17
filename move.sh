52ea000bcb8b7a0de8e92b803e0b02e4f858dc2c
Mona <m.wish95@gmail.com>
2019-04-16 18:59:58 -0700

    modify compile script to use config file

--- move.sh
+++ move.sh
@@ -3,9 +3,7 @@ source ./config.sh
 cd $CODES_PATH
 for i in $(ls -d */); do
 	cd $i
-	echo $i "start"
-	rm run
-	g++ *.cpp -o run
-	echo $i "done"
+	cp $SOLUTION_PATH/*.cpp .
+	cp $SOLUTION_PATH/*.h .
 	cd ..
 done
